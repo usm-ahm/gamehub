89e7bf698886a2201c471eaa68afefff6b88c5ec
Usman <usman_ahm@icloud.com>
2024-08-26 01:01:43 +0500

    style: build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+
 function App() {
-  return <p>Hello World</p>;
+  return (
+    <Grid2 container>
+      <Grid2 xs={12}>
+        <p>Nav</p>
+      </Grid2>
+      <Grid2 lg={3} display={{ xs: "none", lg: "block" }}>
+        <p>Aside</p>
+      </Grid2>
+      <Grid2 lg>
+        <p>Main</p>
+      </Grid2>
+    </Grid2>
+  );
 }
 
 export default App;
