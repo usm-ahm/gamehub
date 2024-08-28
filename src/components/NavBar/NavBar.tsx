c17f38dba934e378412c76c8882120795fce8ac3
Usman <usman_ahm@icloud.com>
2024-08-28 23:42:12 +0500

    refactor: remove redundant switch component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,20 +1,11 @@
 import { Stack, Typography } from "@mui/material";
-import { Switch } from "../Common";
 
 const NavBar = () => {
   return (
-    <Stack
-      component="nav"
-      direction="row"
-      justifyContent="space-between"
-      alignItems="center"
-      role="navigation"
-      aria-label="Main Navigation"
-    >
+    <Stack component="nav" role="navigation" aria-label="Main Navigation">
       <Typography component="h1" variant="h4" fontWeight={900}>
         gamehub
       </Typography>
-      <Switch id="darkModeSwitch" />
     </Stack>
   );
 };
