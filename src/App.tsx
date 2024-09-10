a67cf49dbfeda496eae67c588597890befb8d485
Usman Ahmad <usman.ahm.79@gmail.com>
2024-09-10 17:30:26 +0500

    refactor: use media query hook for responsive GenreList rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { Grid2 } from "@mui/material";
+import { Grid2, Theme, useMediaQuery } from "@mui/material";
 import { NavBar } from "./components/NavBar";
 import { GameGrid } from "./components/Main";
 import { GenreList } from "./components/Aside";
 
 function App() {
+  const isLargeScreen = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.up("lg")
+  );
+
   return (
     <Grid2 container>
       <Grid2 size={{ xs: 12 }}>
         <NavBar />
       </Grid2>
-      <Grid2 size={{ xs: 0, lg: 2 }} display={{ xs: "none", lg: "block" }}>
-        <GenreList />
-      </Grid2>
+      {isLargeScreen && (
+        <Grid2 size={{ lg: 2 }}>
+          <GenreList />
+        </Grid2>
+      )}
       <Grid2 size={{ xs: 12, lg: 10 }}>
         <GameGrid />
       </Grid2>
